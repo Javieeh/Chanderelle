0d1c29a7521d923b5086cb6cf953bb65ae1cefe4
Javieeh <Javierdelaspenas@gmail.com>
2023-01-03 18:35:21 +0100

    Update Handler

--- src/main/java/com/chanderelle/demo/WebSocketHandler.java
+++ src/main/java/com/chanderelle/demo/WebSocketHandler.java
@@ -1,5 +1,20 @@
 package com.chanderelle.demo;
+import java.util.concurrent.atomic.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.Random;
 
-public class WebSocketHandler {
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import java.time.*;
+import java.time.temporal.ChronoUnit;
+
+public class WebSocketHandler extends TextWebSocketHandler {
 
 }
