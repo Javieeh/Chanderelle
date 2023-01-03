9fceb75aeb3c28fcc5f8a3b1757fffefc6481fa5
Javieeh <Javierdelaspenas@gmail.com>
2023-01-03 18:17:24 +0100

    Clase Game

---
+++ src/main/java/com/chanderelle/demo/Game.java
@@ -0,0 +1,51 @@
+package com.chanderelle.demo;
+import org.springframework.web.socket.WebSocketSession;
+
+public class Game {
+	//Atributos
+	private User J1;
+	private User J2;
+	private Boolean full;
+	private Boolean empty;
+	
+	//Constructores
+	Game() {
+		this.J1 = null;
+		this.J2 = null;
+		this.full = false;
+		this.empty = true;
+	}
+	Game(User j1, User j2) {
+		this.J1 = j1;
+		this.J2 = j2;
+		this.full = true;
+		this.empty = false;
+	}
+	//Getters y setters
+	public User getJ1() {
+		return J1;
+	}
+	public void setJ1(User j1) {
+		J1 = j1;
+	}
+	public User getJ2() {
+		return J2;
+	}
+	public void setJ2(User j2) {
+		J2 = j2;
+	}
+	public Boolean getFull() {
+		return full;
+	}
+	public void setFull(Boolean full) {
+		this.full = full;
+	}
+	public Boolean getEmpty() {
+		return empty;
+	}
+	public void setEmpty(Boolean empty) {
+		this.empty = empty;
+	}
+	
+	
+}
---
+++ src/main/java/com/chanderelle/demo/WebSocketHandler.java
@@ -0,0 +1,5 @@
+package com.chanderelle.demo;
+
+public class WebSocketHandler {
+
+}
