d911dd35220cf0a017d7112b4b0a11497a5f05f3
Javieeh <Javierdelaspenas@gmail.com>
2023-01-03 18:35:57 +0100

    Update Handler

--- src/main/java/com/chanderelle/demo/WebSocketHandler.java
+++ src/main/java/com/chanderelle/demo/WebSocketHandler.java
@@ -1,20 +1,31 @@
 package com.chanderelle.demo;
-import java.util.concurrent.atomic.*;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.Random;
 
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
-
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
-import java.time.*;
-import java.time.temporal.ChronoUnit;
 
 public class WebSocketHandler extends TextWebSocketHandler {
 
+	public ObjectMapper mapper = new ObjectMapper();
+	
+	private void handler(TextMessage message, WebSocketSession session) throws JsonMappingException, JsonProcessingException {
+		
+		JsonNode node = mapper.readTree(message.getPayload());
+		int id = node.get("ID").asInt();
+		
+		ObjectNode msgNode = mapper.createObjectNode();
+		ObjectNode msgNodeAux = mapper.createObjectNode();
+		
+		switch(id) {
+		
+		
+		}
+		
+	}
 }
