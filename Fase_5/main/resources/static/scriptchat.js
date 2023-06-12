035e2707a364e86420986cc949c2aa207fab5dfd
Javieeh <81293637+Javieeh@users.noreply.github.com>
2023-06-12 03:02:37 +0200

    Delete Fase 5

--- Fase_5/main/resources/static/scriptchat.js
+++
@@ -1,6 +0,0 @@
-function AddText(){
-    var mensaje = document.getElementById("value-input");
-    var chat = document.getElementById("pdivMESSAGES");
-
-    chat.innerHTML+= mensaje.value+"<br> ";
-}
